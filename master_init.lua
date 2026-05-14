71762818c26b2afeba1e5e824a2796cc61c05d8f
PowahNull <gregnguyen877@gmail.com>
2026-05-14 17:39:26 +0800

    Update master_init.lua
    some more fixes

--- master_init.lua
+++ master_init.lua
@@ -1,65 +1,91 @@
-rednet.open("back")
+-- get modem
+peripheral.find("modem", rednet.open)
+-- loop max iterations to avoid exhaustion
+MAX_ITERATIONS = 100
 
 -- Read disk content for slaves
-local SLAVE_CONNECT_PATH = "disk/slaves.txt"
-local SLAVE_DATA_FILE = fs.open(SLAVE_CONNECT_PATH, "r")
-local SLAVE_JSON = SLAVE_DATA_FILE.readAll()
-SLAVE_DATA_FILE.close()
-local SLAVE_TABLE = textutils.unserialiseJSON(SLAVE_JSON)
-local NUMBER_OF_SLAVES = #SLAVE_TABLE
-SELF_ID = os.getComputerID()
+do
+    SLAVE_CONNECT_PATH = "disk/slaves.json"
+    SLAVE_DATA_FILE = fs.open(SLAVE_CONNECT_PATH, "r")
 
--- Slave computers handshake
-while true do
+    if type(SLAVE_DATA_FILE) == "nil" then
+        print("ERROR: SLAVE DATA FILE NOT FOUND, EXITING")
+        exit()
+    elseif type(SLAVE_DATA_FILE) == "string" then
+        print("ERROR: COULD NOT OPEN FILE, EXITING")
+        exit()
+    end
+
+    SLAVE_JSON = SLAVE_DATA_FILE.readAll()
+    SLAVE_DATA_FILE.close()
+    SLAVE_TABLE = textutils.unserialiseJSON(SLAVE_JSON)
+
+    NUMBER_OF_SLAVES = #SLAVE_TABLE
+    SELF_ID = os.getComputerID()
+end
+
+-- Slave computers detection
+for i = 1, MAX_ITERATIONS do
     local computers = {rednet.lookup("SLAVE_INIT_PROTOCOL")}
     if #computers == NUMBER_OF_SLAVES then
-        print(string.format("found all slave computers: %d/%d registered", #computers, NUMBER_OF_SLAVES))
-        print("performing network handshake...")
+        print(string.format("FOUND ALL SLAVE COMPUTERS: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
+        print("PERFORMING NETWORK HANDSHAKE...")
         break
     elseif #computers > NUMBER_OF_SLAVES then
-        print(string.format("extra slave computers: %d/%d registered", #computers, NUMBER_OF_SLAVES))
-        print("stopping execution to avoid unexpected behavior, goodbye!")
+        print(string.format("EXTRA SLAVE COMPUTERS: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
+        print("EXITING TO AVOID UNEXPECTED BEHAVIOUR")
         exit()
     elseif #computers < NUMBER_OF_SLAVES and #computers > 0 then
-        print(string.format("missing slave computers: %d/%d registered", #computers, NUMBER_OF_SLAVES))
+        print(string.format("MISSING SLAVE COMPUTER: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
     else
-        print(string.format("no slave computers found: %d/%d registered", #computers, NUMBER_OF_SLAVES))
+        print(string.format("NO SLAVE COMPUTERS FOUND: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
     end
 end
 
--- Network handshake
-local all_response_ok = true
+local term_width, term_height = term.getSize()
+print(string.rep("=", term_width))
+
+-- Slave computers handshake
+local response_success = 0
+local response_timeout = 0
+local response_failure = 0
 for _, v in pairs(SLAVE_TABLE) do
     rednet.send(v.slave_id, v.packager, "SLAVE_INIT_PROTOCOL")
     local slave_id, status = rednet.receive("SLAVE_INIT_PROTOCOL", 5)
+
     if not slave_id then
-        status = "HANDSHAKE_TIMEOUT"
+        status = "INIT_TIMEOUT"
     end
 
-    io.write(string.format("Slave #%d responded with: ", v.slave_id))
-    if status == "HANDSHAKE_OK" then
+    io.write(string.format("Slave %d responded with: "))
+    if status == "INIT_OK" then
         term.setTextColor(colors.green)
-    elseif status == "HANDSHAKE_NOT_OK" then
+        response_success = response_success + 1
+    elseif status == "INIT_NOT_OK" then
         term.setTextColor(colors.red)
-        all_response_ok = false
-    elseif status == "HANDSHAKE_TIMEOUT" then
+        response_failure = response_failure + 1
+    elseif status == "INIT_TIMEOUT" then
         term.setTextColor(colors.yellow)
-        all_response_ok = false
+        response_timeout = response_timeout + 1
     end
+
     io.write(status)
     -- terminatate print
     term.setTextColor(colors.white)
     io.write("\n")
 end
-
-if all_response_ok then
-    term.setTextColor(colors.green)
-    print("FULL SLAVE DIAGNOSTICS: NETWORK OK")
-    term.setTextColor(colors.white)
-    _G.NETWORK_OK = all_response_ok
-else
-    term.setTextColor(colors.red)
-    print("FULL SLAVE DIAGNOSTICS: NETWORK NOT OK")
-    term.setTextColor(colors.white)
-    _G.NETWORK_OK = all_response_ok
-end
+print(string.rep("=", term_width))
+io.write("SLAVE NETWORK DIAGNOSTICS REPORT\n")
+io.write("SLAVE INIT SUCCESS: ")
+term.setTextColor(colors.green)
+io.write(string.format("%d/%d (%.1f%%)\n", response_success, NUMBER_OF_SLAVES, response_success/NUMBER_OF_SLAVES))
+term.setTextColor(colors.white)
+io.write("SLAVE INIT TIMEOUT: ")
+term.setTextColor(colors.yellow)
+io.write(string.format("%d/%d (%.1f%%)\n", response_timeout, NUMBER_OF_SLAVES, response_timeout/NUMBER_OF_SLAVES))
+term.setTextColor(colors.white)
+io.write("SLAVE INIT FAILURE: ")
+term.setTextColor(colors.red)
+io.write(string.format("%d/%d (%.1f%%)", response_failure, NUMBER_OF_SLAVES, response_failure/NUMBER_OF_SLAVES))
+term.setTextColor(colors.white)
+print(string.rep("=", term_width))
