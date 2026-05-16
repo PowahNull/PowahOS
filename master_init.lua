5825d7647ca8ab8ca05e54ae31b6fef0ae576109
PowahNull <gregnguyen877@gmail.com>
2026-05-16 18:13:08 +0800

    Update master_init.lua
    removed global variables

--- master_init.lua
+++ master_init.lua
@@ -1,44 +1,45 @@
+local G = {}
 -- get modem
 peripheral.find("modem", rednet.open)
 -- loop max iterations to avoid exhaustion
-MAX_ITERATIONS = 100
+G.MAX_ITERATIONS = 100
 
 -- Read disk content for slaves
 do
-    SLAVE_CONNECT_PATH = "disk/slaves.json"
-    SLAVE_DATA_FILE = fs.open(SLAVE_CONNECT_PATH, "r")
+    G.SLAVE_CONNECT_PATH = "disk/slaves.json"
+    G.SLAVE_DATA_FILE = fs.open(G.SLAVE_CONNECT_PATH, "r")
 
-    if type(SLAVE_DATA_FILE) == "nil" then
+    if type(G.SLAVE_DATA_FILE) == "nil" then
         error("SLAVE DATA FILE NOT FOUND", 0)
-    elseif type(SLAVE_DATA_FILE) == "string" then
+    elseif type(G.SLAVE_DATA_FILE) == "string" then
         error("COULD NOT OPEN FILE", 0)
     end
 
-    SLAVE_JSON = SLAVE_DATA_FILE.readAll()
-    SLAVE_DATA_FILE.close()
-    SLAVE_TABLE = textutils.unserialiseJSON(SLAVE_JSON)
+    G.SLAVE_JSON = G.SLAVE_DATA_FILE.readAll()
+    G.SLAVE_DATA_FILE.close()
+    G.SLAVE_TABLE = textutils.unserialiseJSON(G.SLAVE_JSON)
 
-    if not SLAVE_TABLE then
+    if not G.SLAVE_TABLE then
         error("SLAVE DATA JSON PARSE FAILURE", 0)
     end
 
-    NUMBER_OF_SLAVES = #SLAVE_TABLE
-    SELF_ID = os.getComputerID()
+    G.NUMBER_OF_SLAVES = #G.SLAVE_TABLE
+    G.SELF_ID = os.getComputerID()
 end
 
 -- Slave computers detection
-for i = 1, MAX_ITERATIONS do
+for i = 1, G.MAX_ITERATIONS do
     local computers = {rednet.lookup("SLAVE_INIT_PROTOCOL")}
-    if #computers == NUMBER_OF_SLAVES then
-        print(string.format("FOUND ALL SLAVE COMPUTERS: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
+    if #computers == G.NUMBER_OF_SLAVES then
+        print(string.format("FOUND ALL SLAVE COMPUTERS: %d/%d REGISTERED", #computers, G.NUMBER_OF_SLAVES))
         print("PERFORMING NETWORK HANDSHAKE...")
         break
-    elseif #computers > NUMBER_OF_SLAVES then
-        error(string.format("UNEXPECTED SLAVE COMPUTERS: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES), 0)
-    elseif #computers < NUMBER_OF_SLAVES and #computers > 0 then
-        warn(string.format("MISSING SLAVE COMPUTER: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
+    elseif #computers > G.NUMBER_OF_SLAVES then
+        error(string.format("UNEXPECTED SLAVE COMPUTERS: %d/%d REGISTERED", #computers, G.NUMBER_OF_SLAVES), 0)
+    elseif #computers < G.NUMBER_OF_SLAVES and #computers > 0 then
+        warn(string.format("MISSING SLAVE COMPUTER: %d/%d REGISTERED", #computers, G.NUMBER_OF_SLAVES))
     else
-        warn(string.format("NO SLAVE COMPUTERS FOUND: %d/%d REGISTERED", #computers, NUMBER_OF_SLAVES))
+        warn(string.format("NO SLAVE COMPUTERS FOUND: %d/%d REGISTERED", #computers, G.NUMBER_OF_SLAVES))
     end
 end
 
@@ -49,7 +50,7 @@ print(string.rep("=", term_width))
 local response_success = 0
 local response_timeout = 0
 local response_failure = 0
-for _, v in pairs(SLAVE_TABLE) do
+for _, v in pairs(G.SLAVE_TABLE) do
     rednet.send(v.slave_id, v.packager, "SLAVE_INIT_PROTOCOL")
     local slave_id, status = rednet.receive("SLAVE_INIT_PROTOCOL", 5)
 
@@ -78,16 +79,16 @@ print(string.rep("=", term_width))
 io.write("SLAVE NETWORK DIAGNOSTICS REPORT\n")
 io.write("SLAVE INIT SUCCESS: ")
 term.setTextColor(colors.green)
-io.write(string.format("%d/%d (%.1f%%)\n", response_success, NUMBER_OF_SLAVES, response_success/NUMBER_OF_SLAVES))
+io.write(string.format("%d/%d (%.1f%%)\n", response_success, G.NUMBER_OF_SLAVES, response_success/G.NUMBER_OF_SLAVES))
 term.setTextColor(colors.white)
 io.write("SLAVE INIT TIMEOUT: ")
 term.setTextColor(colors.yellow)
-io.write(string.format("%d/%d (%.1f%%)\n", response_timeout, NUMBER_OF_SLAVES, response_timeout/NUMBER_OF_SLAVES))
+io.write(string.format("%d/%d (%.1f%%)\n", response_timeout, G.NUMBER_OF_SLAVES, response_timeout/G.NUMBER_OF_SLAVES))
 term.setTextColor(colors.white)
 io.write("SLAVE INIT FAILURE: ")
 term.setTextColor(colors.red)
-io.write(string.format("%d/%d (%.1f%%)\n", response_failure, NUMBER_OF_SLAVES, response_failure/NUMBER_OF_SLAVES))
-if response_success == NUMBER_OF_SLAVES then
+io.write(string.format("%d/%d (%.1f%%)\n", response_failure, G.NUMBER_OF_SLAVES, response_failure/G.NUMBER_OF_SLAVES))
+if response_success == G.NUMBER_OF_SLAVES then
     term.setTextColor(colors.lightBlue)
     print("ALL SYSTEMS OPERATIONAL")
 end
