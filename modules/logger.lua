cb12805a38601776699516a6345087df56d0a1c1
PowahNull <gregnguyen877@gmail.com>
2026-05-27 22:51:53 +0800

    Create logger.lua
    Signed-off-by: PowahNull <gregnguyen877@gmail.com>

---
+++ modules/logger.lua
@@ -0,0 +1,37 @@
+local function timestamp(msg)
+    local day = os.day()
+    local time = textutils.formatTime(os.time(), true)
+    local format = string.format("Day %d %s | %s", day, time, msg)
+    return format
+end
+
+return {
+    fatal_error = function(message)
+        error(timestamp(message), 0)
+    end,
+
+    error = function(message)
+        term.setTextColor(colors.orange)
+        print(timestamp(message))
+    end,
+
+    warn = function(message)
+        term.setTextColor(colors.yellow)
+        print(timestamp(message))
+    end,
+
+    ok = function(message)
+        term.setTextColor(colors.green)
+        print(timestamp(message))
+    end,
+
+    system = function(message)
+        term.setTextColor(colors.blue)
+        print(timestamp(message))
+    end,
+
+    default = function(message)
+        term.setTextColor(colors.white)
+        print(timestamp(message))
+    end
+}
